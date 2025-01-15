56fd32bb95b7bb9d133f9297e52e0e856a5ae5d9
Yuji Nakakoshi <yuji.nkks@gmail.com>
2025-01-15 22:06:25 +0900

    コメントの追加

--- sample-theatre-js/js/production-animation-tutorial.js
+++ sample-theatre-js/js/production-animation-tutorial.js
@@ -1,116 +1,80 @@
 // only import core
-import 'https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.5.0-insiders.88df1ef/dist/core-only.min.js'
+import 'https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.7.2/dist/core-only.min.js'
 // We can now access just Theatre.core from here
+const { core } = Theatre;
 
-const articleHeadingElement = document.getElementById('article-heading')
+const projectId = apex.item("P1_PROJECT_ID").getValue();
+apex.debug.info("projectId: ", projectId);
 
-// Exported by clicking the project name and "Export Project Name to JSON" button.
-const projectState = {
-  sheetsById: {
-    'Sheet 1': {
-      staticOverrides: {
-        byObject: {
-          'Heading 1': {
-            opacity: 0.9936708860759493,
-          },
-        },
-      },
-      sequence: {
-        subUnitsPerUnit: 30,
-        length: 10,
-        type: 'PositionalSequence',
-        tracksByObject: {
-          'Heading 1': {
-            trackData: {
-              LrJ3eujCAE: {
-                type: 'BasicKeyframedTrack',
-                keyframes: [
-                  {
-                    id: 'i-s2GT5JFm',
-                    position: 0,
-                    connectedRight: true,
-                    handles: [0.5, 1, 0.882, 0],
-                    value: 0,
-                  },
-                  {
-                    id: 'I3Suv35jmV',
-                    position: 3.433,
-                    connectedRight: true,
-                    handles: [0.055, 0.969, 0.82, -0.031],
-                    value: 96,
-                  },
-                  {
-                    id: 'M_00uVAeuK',
-                    position: 5.6,
-                    connectedRight: true,
-                    handles: [0.087, 1.01, 0.5, 0],
-                    value: 0,
-                  },
-                ],
-              },
-              '9JzZUrUSb7': {
-                type: 'BasicKeyframedTrack',
-                __debugName: 'Heading 1:["opacity"]',
-                keyframes: [
-                  {
-                    id: 'EfAPcjrYAy',
-                    position: 0,
-                    connectedRight: true,
-                    handles: [0.5, 1, 0.5, 0],
-                    value: 0.9936708860759493,
-                  },
-                  {
-                    id: 'hZ-tUkMP4C',
-                    position: 2.367,
-                    connectedRight: true,
-                    handles: [0.5, 1, 0.5, 0],
-                    value: 0,
-                  },
-                  {
-                    id: 'D5PA_XGfS6',
-                    position: 5.6,
-                    connectedRight: true,
-                    handles: [0.5, 1, 0.5, 0],
-                    value: 0.9936708860759493,
-                  },
-                ],
-              },
-            },
-            trackIdByPropPath: {
-              '["y"]': 'LrJ3eujCAE',
-              '["opacity"]': '9JzZUrUSb7',
-            },
-          },
-        },
-      },
-    },
+/*
+* Theatre.jsのアニメーションを表示する処理。
+* 
+* コード自体はGetting StartedのWith HTML/SVGに記載されているものだが、
+* 処理はボタンIMPORTを押した時に実行されるように、ファンクションにしている。
+* https://www.theatrejs.com/docs/latest/getting-started/with-html-svg
+*/
+async function restoreProjectState( projectId ) {
+  const articleHeadingElement = document.getElementById('article-heading');
+
+  /*
+  * アニメーションを定義しているJSONは、データベースから取り出す。
+  */
+  const response = await apex.server.process(
+    "RESTORE_PROJECT_STATE",
+    {
+      x01: projectId
+    }
+  );
+  const projectState = JSON.parse(response.state);
+  apex.debug.info("projectState: ", projectState);
+
+  /*
+  * 以下はGetting StartedのWith HTML/SVGのコード
+  */
+  const project = core.getProject( projectId, {
+    state: projectState,
+  });
+  
+  const sheet = project.sheet('Sheet 1');
+  const obj = sheet.object('Heading 1', {
+    y: 0,
+    opacity: core.types.number(1, { range: [0, 1] }),
+  });
+  
+  // animations
+  obj.onValuesChange((obj) => {
+    articleHeadingElement.style.transform = `translateY(${obj.y}px)`
+    articleHeadingElement.style.opacity = obj.opacity
+  });
+  
+  // wait for project to be ready
+  project.ready.then(() => {
+    apex.debug.info('project is ready', project);
+    sheet.sequence.play({ iterationCount: Infinity })
+  });
+};
+
+/*
+* Theatre.jsのアニメーションのエクスポートとインポートを行う。
+*/
+const channel = new BroadcastChannel('control-theatre');
+
+const controlElement = document.getElementById("CONTROLS");
+const controls = apex.actions.createContext("controls", controlElement);
+
+controls.add([
+  {
+    // EXPORTはStudioが実装されているページで処理する。
+    name: "EXPORT",
+    action: (event, element, args) => {
+      channel.postMessage( { type: "export" } );
+    }
   },
-  definitionVersion: '0.4.0',
-  revisionHistory: ['vLg01lxRrpP8eGsS'],
-}
-
-const { core } = Theatre
-
-// We don't need studio in production
-// studio.initialize()
-
-const project = core.getProject('HTML Animation Tutorial', {
-  state: projectState,
-})
-
-const sheet = project.sheet('Sheet 1')
-const obj = sheet.object('Heading 1', {
-  y: 0,
-  opacity: core.types.number(1, { range: [0, 1] }),
-})
-
-// animations
-obj.onValuesChange((obj) => {
-  articleHeadingElement.style.transform = `translateY(${obj.y}px)`
-  articleHeadingElement.style.opacity = obj.opacity
-})
-
-// wait for project to be ready
-project.ready.then(() => {
-  sheet.sequence.play({ iterationCount: Infinity, range: [0, 6] })
-})
+  {
+    // IMPORTはこのページで処理する。
+    name: "IMPORT",
+    action: (event, element, args) => {
+      restoreProjectState( projectId );
+    }
+  }
+]);
--- sample-theatre-js/js/theatre-studio.js
+++ sample-theatre-js/js/theatre-studio.js
@@ -1,19 +1,64 @@
-import 'https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.5.0-insiders.88df1ef/dist/core-and-studio.js'
+import 'https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.7.2/dist/core-and-studio.js';
 // We can now access Theatre.core and Theatre.studio from here
-const { core, studio } = Theatre
+const { core, studio } = Theatre;
 
-studio.initialize() // Start the Theatre.js UI
+const projectId = apex.item("P2_PROJECT_ID").getValue();
+apex.debug.info("projectId: ", projectId);
 
-const project = core.getProject('HTML Animation Tutorial')
+/*
+* Theatre.jsのStudioを使用するための処理。
+* 
+* 以下のGetting StartedのWith HTML/SVGのコードそのままです。
+* https://www.theatrejs.com/docs/latest/getting-started/with-html-svg
+*/
+studio.initialize(); // Start the Theatre.js UI
+
+const project = core.getProject(projectId);
+
 const sheet = project.sheet('Sheet 1')
 const obj = sheet.object('Heading 1', {
   y: 0, // you can use just a simple default value
   opacity: core.types.number(1, { range: [0, 1] }), // or use a type constructor to customize
-})
+});
 
-const articleHeadingElement = document.getElementById('article-heading')
+const articleHeadingElement = document.getElementById('article-heading');
 
 obj.onValuesChange((obj) => {
   articleHeadingElement.style.transform = `translateY(${obj.y}px)`
   articleHeadingElement.style.opacity = obj.opacity
-})
+});
+
+/*
+* BroadcastChannelからイベントを受け取って、アニメーションをエクスポートする。
+* 本サンプルで追加したデータベースへアニメーションを保存する処理。
+*/
+const channel = new BroadcastChannel('control-theatre');
+
+channel.addEventListener("message", (event) => {
+  apex.debug.info("event: ", event);
+  if (event.data.type === "export") {
+    /*
+    * 現在のアニメーションの設定をJSONドキュメントとして取り出し。
+    * サーバーに送信するためにページ・アイテムP2_PROJECT_STATEに
+    * 設定する。
+    */
+    const projectState = studio.createContentOfSaveFile(projectId);
+    apex.debug.info("projectState: ", projectState);
+    apex.item("P2_PROJECT_STATE").setValue(JSON.stringify(projectState));
+
+    /*
+    * アニメーションをデータベースに保存する。
+    */
+    apex.server.process(
+      "SAVE_PROJECT_STATE",
+      {
+        pageItems: "#P2_PROJECT_ID,#P2_PROJECT_STATE"
+      },
+      {
+        success: (data) => {
+          apex.debug.info(data);
+        }
+      }
+    )
+  }
+});
